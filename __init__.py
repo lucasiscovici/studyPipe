39971a9f8d0f8f2a788722c24380ca1fb1a04f15
lucas iscovici <lucasiscovici@macbookprolulu.home>
2019-10-28 17:52:31 +0100

    studyPipeGit-Lun 28 oct 2019 17:52:31 CET

--- __init__.py
+++ __init__.py
@@ -1,2 +1,8 @@
 #ICI
-from .studyPipe import *
+from .studyPipe import config
+from .studyPipe import __, _fun_,_funs_, _ftools_, _funsInv_
+from .studyPipe import __fun__, __funs__, __funsInv__
+from .studyPipe import px, _px, px_, p, _p, p_ #sspipe
+from .studyPipe import X, _X, X_, df, _df, df_ #dfply
+from .studyPipe import curried, _c, c_ #toolz.curried
+from .studyPipe import T, F
---
+++ studyPipe/pipes.py
@@ -0,0 +1,4 @@
+from . import __, _fun_, _funs_, _funsInv_
+from . import __fun__, __funs__, __funsInv__
+from . import _ftools_
+ 
