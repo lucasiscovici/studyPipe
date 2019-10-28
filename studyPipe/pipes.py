d9189f1299344c101a9fcca2be19c884fd9a3660
lucas iscovici <lucasiscovici@macbookprolulu.home>
2019-10-28 18:18:17 +0100

    studyPipeGit-Lun 28 oct 2019 18:18:17 CET

--- studyPipe/pipes.py
+++ studyPipe/pipes.py
@@ -2,3 +2,6 @@ from . import __, _fun_, _funs_, _funsInv_
 from . import __fun__, __funs__, __funsInv__
 from . import _ftools_
  
+ __all__=["__","_fun_","_funs_","_funsInv_",\
+ 		 "__fun__","__funs__","__funsInv__",\
+ 		 "_ftools_"]
