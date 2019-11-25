99215078fe5fe409c91a70fe0260e6fd6d30c2fc
Your Name <you@example.com>
2019-11-25 15:51:10 +0100

    studyPipeGit-Lun 25 nov 2019 15:51:10 CET

--- studyPipe/__init__.py
+++ studyPipe/__init__.py
@@ -18,4 +18,4 @@ from .studyPipe import X, _X, X_, df, _df, df_ #dfply
 from .studyPipe import curried, _c, c_ #toolz.curried
 from .studyPipe import T, F
 
-__all__=__all__+["configureStudyPipe","_fun_","_funs_", "_ftools_", "_funsInv_","__fun__", "__funs__", "__funsInv__","px", "_px", "px_", "p", "_p", "p_","X", "_X", "X_", "df", "_df", "df_","curried", "_c", "c_","T",F]
+__all__=__all__+["configureStudyPipe","_fun_","_funs_", "_ftools_", "_funsInv_","__fun__", "__funs__", "__funsInv__","px", "_px", "px_", "p", "_p", "p_","X", "_X", "X_", "df", "_df", "df_","curried", "_c", "c_","T","F"]
