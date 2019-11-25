86a01c226ddd3345ad86308a5129f55aa92644b4
Your Name <you@example.com>
2019-11-25 15:50:14 +0100

    studyPipeGit-Lun 25 nov 2019 15:50:14 CET

--- studyPipe/__init__.py
+++ studyPipe/__init__.py
@@ -1,11 +1,16 @@
 from .studyPipe import configureStudyPipe
-
+import warnings
 try:
   import google.colab
   from .studyPipe import __ as _p_
+  __all__=["_p_"]
+  warnings.warn("""
+In Google Colab: __ become _p_
+""")
 except:
   from .studyPipe import __
-  
+  __all__=["__"]
+
 from .studyPipe import _fun_,_funs_, _ftools_, _funsInv_
 from .studyPipe import __fun__, __funs__, __funsInv__
 from .studyPipe import px, _px, px_, p, _p, p_ #sspipe
@@ -13,3 +18,4 @@ from .studyPipe import X, _X, X_, df, _df, df_ #dfply
 from .studyPipe import curried, _c, c_ #toolz.curried
 from .studyPipe import T, F
 
+__all__=__all__+["configureStudyPipe","_fun_","_funs_", "_ftools_", "_funsInv_","__fun__", "__funs__", "__funsInv__","px", "_px", "px_", "p", "_p", "p_","X", "_X", "X_", "df", "_df", "df_","curried", "_c", "c_","T",F]
